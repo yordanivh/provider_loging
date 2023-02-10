b83a7c255bcdea006953b9c29b1f0b5163eb366c
Yordan Halachev <yordan.i.halachev@gmail.com>
2023-02-10 17:38:52 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "vsphere" {
+  # If you use a domain set your login like this "MyDomain\\MyUser"
+  user           = "user"
+  password       = "password"
+  vsphere_server = "192.168.1.8"
+  client_debug = true
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
